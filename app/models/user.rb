7e2e2fd7f2d8ce54f7850d510f9dcd318e808e99
Kevin Storberg <kevinstorberg@Kevins-MacBook-Pro.local>
2016-08-10 09:19:58 -0400

    set up logic for user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  validates :first_name, :last_name, :password, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :role, :inclusion => { :in => %{ admin } }, :allow_nil => true
+  has_secure_password
+
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
