33883990a7cc2bdf4511efbc9ea1a836421ed7c0
Kevin Storberg <kevinstorberg@Kevins-MacBook-Pro.local>
2016-08-13 01:35:57 -0400

    created blog assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
 
+  get 'blogs/index'
+
+  get 'blogs/show'
+
+  get 'blogs/new'
+
+  get 'blogs/edit'
+
+  get 'blogs/create'
+
+  get 'blogs/update'
+
+  get 'blogs/destroy'
+
   root 'welcome#index'
 
   resources :users
