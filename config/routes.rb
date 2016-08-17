a78b5973a547a820a05539c5ac7aefba017dc1b5
Kevin Storberg <kevinstorberg@Kevins-MacBook-Pro.local>
2016-08-17 17:49:33 -0400

    created and styled paths in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,30 @@
 Rails.application.routes.draw do
 
-  get 'pages/about'
-
-  get 'pages/contact'
-
+  # Root ===================================================================
   root 'welcome#index'
 
+
+  # Users ===================================================================
   resources :users
 
+
+  # Sessions ===================================================================
   resources :sessions, only: [:new, :create, :destroy]
     get "signup", to: "users#new", as: "signup"
     get "login", to: "sessions#new", as: "login"
     get "logout", to: "sessions#destroy", as: "logout"
 
+
+  # Blogs ===================================================================
   resources :blogs
 
-  resources :comments 
+
+  # Comments ===================================================================
+  resources :comments
+
+
+  # Pages ===================================================================
+  get 'about', to: 'pages#about', as: 'about'
+  get 'contact', to: 'pages#contact', as: 'contact'
 
 end
